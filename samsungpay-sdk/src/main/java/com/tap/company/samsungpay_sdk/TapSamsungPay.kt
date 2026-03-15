2abbf580a32023e4075e238cbc98c6eaf9032798
Ahlaam K <a.kazi@tap.company>
2026-03-15 15:11:44 +0300

    handling of the views

--- samsungpay-sdk/src/main/java/com/tap/company/samsungpay_sdk/TapSamsungPay.kt
+++ samsungpay-sdk/src/main/java/com/tap/company/samsungpay_sdk/TapSamsungPay.kt
@@ -407,7 +407,11 @@ class TapSamsungPay : LinearLayout, ApplicationLifecycle {
             samsungCheckoutStarted = false
             SamsungPayDataConfiguration.getTapCardStatusListener()?.onSamsungPayCancel()
             Log.e("SamsungPay", "Sheet was closed/canceled")
+            init(cardConfiguraton)
+            cardWebview?.post {
+                cardWebview?.visibility = View.VISIBLE
 
+            }
           /*  val launchIntent = context.packageManager.getLaunchIntentForPackage(context.packageName)
             launchIntent?.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT)
             context.startActivity(launchIntent)*/
